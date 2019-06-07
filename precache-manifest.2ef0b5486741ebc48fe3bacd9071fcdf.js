603fe53486fcf8645c55c304e20a076254b22e1e
Igori4 <igorpilyak@gmail.com>
2019-06-07 21:49:49 +0300

    Updates

---
+++ precache-manifest.2ef0b5486741ebc48fe3bacd9071fcdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a269a17d99654fd66f3da6e0c51327",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "e25d93ee90cf4fa8e74e",
+    "url": "/tic-tac-toe/static/css/main.95940393.chunk.css"
+  },
+  {
+    "revision": "1439f38f9ece51aaf058",
+    "url": "/tic-tac-toe/static/js/2.fde396be.chunk.js"
+  },
+  {
+    "revision": "e25d93ee90cf4fa8e74e",
+    "url": "/tic-tac-toe/static/js/main.7eba69b3.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
