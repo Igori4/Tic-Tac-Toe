a47599d8d49b758f2db3471fc1d75170356ec34b
Igori4 <igorpilyak@gmail.com>
2019-09-04 14:40:19 +0300

    implement mobile size version

--- src/Board.js
+++ src/Board.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import Cell from './Cell'
+import React from "react";
+import Cell from "./Cell";
 class Board extends React.Component {
-
-    renderCells() {
-        const {cells, onClick} = this.props;
-        return cells.map( (cell , index) => {
-            return (
-              <Cell 
-              value = {cell}
-              key = {'Cell -' + index}
-              onClick = {() => {onClick(index)} }
-              />
-            )
-        })
+  constructor(props) {
+    super(props)
+  }
+  renderResetButton(winner, stepCounter) {
+    if (winner || stepCounter === 10) {
+      return (
+        <button className="resetGame" onClick={this.props.restartGame}>
+          Restart Game
+        </button>
+      );
     }
+  }
+  renderCells() {
+    const { cells, onClick } = this.props;
+    return cells.map((cell, index) => {
+      return (
+        <Cell
+          value={cell}
+          key={"Cell -" + index}
+          onClick={() => {
+            onClick(index);
+          }}
+        />
+      );
+    });
+  }
 
-
-    render() {
-        return (
-        <div className="board">
-          {this.renderCells()}  
-        </div>
-        )
-    }
+  render() {
+    const {winner,stepCounter} = this.props;
+    return (
+      <div className="board">
+        {this.renderCells()} {this.renderResetButton(winner, stepCounter)}
+      </div>
+    );
+  }
 }
-
 
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import Board from './Board';
-import getWinner from './utils';
+import React from "react";
+import "./App.css";
+import Board from "./Board";
+import getWinner from "./utils";
 
 class Game extends React.Component {
   constructor() {
@@ -10,69 +10,65 @@ class Game extends React.Component {
     this.state = {
       cells: Array(9).fill(null),
       stepCounter: 1,
-      whoNext:'X',
+      whoNext: "X",
       winner: null
-    }
+    };
   }
 
-  hendlerCells = (index) => {
-    let {cells, whoNext, stepCounter, winner} = this.state;
-      cells = [...cells];
-      if(cells[index] || winner) {
-        return;
-      }
-      cells[index] = whoNext;
-      whoNext = whoNext === 'X' ? '0' : 'X';
-      stepCounter = stepCounter + 1;
-      winner = getWinner(cells);
-      this.setState({
-        cells,
-        whoNext,
-        stepCounter,
-        winner
-      }) 
-  }
+  hendlerCells = index => {
+    let { cells, whoNext, stepCounter, winner } = this.state;
+    cells = [...cells];
+    if (cells[index] || winner) {
+      return;
+    }
+    cells[index] = whoNext;
+    whoNext = whoNext === "X" ? "0" : "X";
+    stepCounter = stepCounter + 1;
+    winner = getWinner(cells);
+    this.setState({
+      cells,
+      whoNext,
+      stepCounter,
+      winner
+    });
+  };
 
   restartGame = () => {
     this.setState({
       cells: Array(9).fill(null),
       stepCounter: 1,
-      whoNext:'X',
+      whoNext: "X",
       winner: null
-    })
-  }
-
-  renderResetButton(winner, stepCounter) {
-    if(winner || stepCounter === 10) {
-      return (
-        <button className="resetGame" onClick = {this.restartGame}>Restart Game</button>
-      )
-    }
-  }
+    });
+  };
 
   wichPlayerIs(whoNext) {
-    return whoNext === 'X' ? "X" : "0";
+    return whoNext === "X" ? "X" : "0";
   }
 
-
-  render( ) {
-    const {cells, winner, whoNext, stepCounter} = this.state;
+  render() {
+    const { cells, winner, whoNext, stepCounter } = this.state;
     let gameStatus;
-    if(winner) {
+    if (winner) {
       gameStatus = `Player ${winner} wins!`;
-    } else if( stepCounter === 10) {
-      gameStatus = 'It is A DRAW!'
+    } else if (stepCounter === 10) {
+      gameStatus = "It is A DRAW!";
     } else {
-      gameStatus = `Player ${whoNext} your turn!`
+      gameStatus = `Player ${whoNext} your turn!`;
     }
     return (
       <div className="wraper">
         <div className="Game">
           <h1>TIC TAC TOE</h1>
           <h3>{gameStatus}</h3>
-          <Board  cells = {cells} onClick ={this.hendlerCells}/>
+          <Board
+            restartGame={this.restartGame}
+            cells={cells}
+            onClick={this.hendlerCells}
+            winner={winner}
+            stepCounter={stepCounter}
+          />
         </div>
-        {this.renderResetButton(winner, stepCounter)}
       </div>
     );
   }
